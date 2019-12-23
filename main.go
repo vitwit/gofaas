9298fc8ade79eb96358146868a4cbf6a3e3d366a
PrathyushaLakkireddy <prathyusha@vitwit.com>
2019-12-23 16:28:37 +0530

    Added code to check alert data

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/kataras/golog"
+	"github.com/vitwit/go-fass/openfass-go/models"
 	"github.com/vitwit/go-fass/openfass-go"
 )
 
@@ -19,4 +20,54 @@ func main() {
 
 	golog.Info("response og get system...........", res)
 
+	data := models.FunctionDefintion{
+		Service:    "nodeinfo123",
+		Network:    "func_functions",
+		Image:      "functions/nodeinfo:latest",
+		EnvProcess: "node main.js",
+		EnvVars: models.EnvVars{
+			AdditionalProp1: "string",
+			AdditionalProp2: "string",
+			AdditionalProp3: "string",
+		},
+		Constraints: []string{
+			"node.platform.os == linux",
+		},
+		Labels: map[string]string{
+			"example": "func1",
+		},
+		Annotations: models.Annotations{
+			Topics: "awesome-kafka-topic",
+			Foo:    "some",
+		},
+		RegistryAuth: "dXNlcjpwYXNzd29yZA==",
+		Limits: models.Limits{
+			Memory: "128M",
+			CPU:    "0.01",
+		},
+		Requests: models.Requests{
+			Memory: "128M",
+			CPU:    "0.01",
+		},
+		ReadOnlyRootFilesystem: true,
+	}
+
+	clires, err := cli.CreateSystemFunctions(data)
+	if err != nil {
+		golog.Error("Error while creating system ", err)
+	}
+
+	golog.Info("response of create system....", clires)
+
+	dd := map[string]string{
+		"receiver": "scale-up",
+		"status": "firing",
+		"alertName":"check",
+	}
+
+	rr, _ := cli.SystemAlert(dd)
+	golog.Info("alert info.......",rr)
+
+	//corsRouter := cors.New(corsRouter11).Handler(router)
+	//log.Fatal(http.ListenAndServe(":3000", nil))
 }
